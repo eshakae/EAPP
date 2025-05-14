657fe6521cac2119582c8a1a24ea80d30958dd6b
Keisha Rainuri Wihamdy <keisharainuri17@gmail.com>
2025-05-14 08:39:34 +0700

    Update siswa.php

--- siswa.php
+++ siswa.php
@@ -6,7 +6,7 @@ if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'siswa') {
 }
 ?>
 <html>
-<head><link rel="stylesheet" href="css/style.css"></head>
+<head><link rel="stylesheet" href="style.css"></head>
 <body>
 <h2>Selamat datang, <?= $_SESSION['username'] ?>!</h2>
 <p><a href="pendaftaran.php">Daftar Ekstrakurikuler</a></p>
